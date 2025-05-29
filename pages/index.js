bb9d7db2f5ccc0d8ebe49978630605842702f8e6
Destray32 <kubaj38@gmail.com>
2025-05-29 17:30:56 +0200

    Add meta tags for SEO and create robots.txt for web crawling

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,22 @@ export default function Home() {
 				<title>Anime Image Board</title>
 				<meta
 					name="description"
-					content="Browse anime images from Waifu.pics API"
+					content="Browse anime images from Waifu.pics API. Best nekko waifu images and anime artwork."
 				/>
+				<meta name="google-site-verification" content="bVP-jy034Vc_iLAOUtk93hs9h2GpnJG-0cMjHh3AtmU" />
 				<link rel="icon" href="/favicon.ico" />
+
+                <meta name="keywords" content="anime, waifu, anime images, anime gallery, waifu pics, anime artwork" />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta name="author" content="TopWaifu" />
+
+				<meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Anime Image Board" />
+                <meta name="twitter:description" content="Browse anime images from Waifu.pics API" />
+
+				<link rel="canonical" href="https://image-board-indol.vercel.app" />
+                
 			</Head>
 
 			<main className={styles.main}>
