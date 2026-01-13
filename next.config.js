f2d5f0805a2df7da5e63307b872ecaa49557cc92
Destray32 <kubaj38@gmail.com>
2026-01-13 22:32:45 +0100

    feat: usuniecie redirect

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'image-board-indol.vercel.app',
-          },
-        ],
-        destination: 'https://animeimages.dpdns.org/:path*',
-        permanent: true, // = 308 (SEO jak 301)
-      },
-    ]
-  },
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async redirects() {
+//     return [
+//       {
+//         source: '/:path*',
+//         has: [
+//           {
+//             type: 'host',
+//             value: 'image-board-indol.vercel.app',
+//           },
+//         ],
+//         destination: 'https://animeimages.dpdns.org/:path*',
+//         permanent: true, // = 308 (SEO jak 301)
+//       },
+//     ]
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
